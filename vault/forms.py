6c3e83ea05c384b7616b7fddbc31137919603d99
uchchash <uchchashtr@gmail.com>
2025-12-13 23:40:46 +0600

    Added Tailwindcss Attrs for login and registration

--- vault/forms.py
+++ vault/forms.py
@@ -6,6 +6,17 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('email', 'first_name', 'last_name')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'w-full px-3 py-2 border rounded-lg focus:ring focus:ring-blue-200'
+            field.widget.attrs['placeholder'] = field.label    
 
 class LoginForm(AuthenticationForm):
     username = forms.EmailField(label='Email') 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'w-full px-3 py-2 border rounded-lg focus:ring focus:ring-blue-200'
+            field.widget.attrs['placeholder'] = field.label    
