872adc316abaf9fd655063c442500d72e72d6864
uchchash <uchchashtr@gmail.com>
2025-12-16 22:48:05 +0600

    Added Shared Folder and Shared File Models
    Shared items require a unique token which is created by calling a function that creates random strings for URLs.

--- sharing/models.py
+++ sharing/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from vault.utils import generate_permalink
+from storage.models import File, Folder
 
-# Create your models here.
+# Create Shared Folder Model
+
+class SharedFolder(models.Model):
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    token = models.CharField(max_length=20, default=generate_permalink, unique=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.folder.name} by {self.folder.user}"
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['folder', 'token']),
+        ]
+
+# Create Shared File Model
+
+class SharedFile(models.Model):
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    token = models.CharField(max_length=20, default=generate_permalink, unique=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.file.name} by {self.file.user}"
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['file', 'token']),
+        ]
---
+++ sharing/urls.py
@@ -0,0 +1,4 @@
+import django.urls as path
+
+urlpatterns = [
+]
